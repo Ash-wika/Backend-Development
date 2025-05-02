02ee4ddfe15b100a8d699b45f13f9faa1783d698
Ash-wika <2k22.cse.2211647@gmail.com>
2025-05-02 16:10:26 +0530

    Example

---
+++ script.js
@@ -0,0 +1,10 @@
+// Fundamentals of JavaScript:
+// arrays and objects
+// functions return
+// async js coding
+
+var arr=[1,2,3,4];
+// for each map filter find indexOf
+arr.forEach(function(val){
+    console.log(val+" hello");
+})
